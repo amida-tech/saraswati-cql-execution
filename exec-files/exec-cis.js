26d5224ffe0f29734410f4a334e816ac3bf2c359
tylernoblett <tyler.noblett@outlook.com>
2021-01-21 16:18:14 -0500

    modify file type for easier data ingestion

--- exec-files/exec-cis.js
+++ exec-files/exec-cis.js
@@ -1,6 +1,6 @@
 const codeservice = require('../data/codes/cis-codes');
 const executor = require('./exec-template');
 const measure = require('../json-elm/cis.json');
-const patients = require('../data/patients/cis-patients');
+const patients = require('../data/patients/cis-patients.json');
 
 executor('2018-12-31', '2019-12-31', measure, patients, codeservice);
