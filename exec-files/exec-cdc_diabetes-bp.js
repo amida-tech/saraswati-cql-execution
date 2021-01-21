b85c9d57ebafd8d23ed4b98970b8becd996ec0dc
tylernoblett <tyler.noblett@outlook.com>
2021-01-21 16:05:36 -0500

    change JS to JSON because data pipeline can more easily create JSON

--- exec-files/exec-cdc_diabetes-bp.js
+++ exec-files/exec-cdc_diabetes-bp.js
@@ -1,6 +1,6 @@
 const codeservice = require('../data/codes/cdc_diabetes-bp-codes');
 const executor = require('./exec-template');
 const measure = require('../json-elm/cdc_diabetes-bp.json');
-const patients = require('../data/patients/cdc_diabetes-bp-patients');
+const patients = require('../data/patients/cdc_diabetes-bp-patients.json');
 
 executor('2018-12-31', '2019-12-31', measure, patients, codeservice);
