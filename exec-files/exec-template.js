09e93eb3b602936370b2055a5651956e7cd3a18b
tylernoblett <tyler.noblett@outlook.com>
2021-01-26 09:58:48 -0500

    convert data into the format James requested

--- exec-files/exec-template.js
+++ exec-files/exec-template.js
@@ -1,5 +1,6 @@
-const fs = require('fs');
+var fs = require('fs');
 const cqlfhir = require('cql-exec-fhir');
+var jsonl = require('jsonl');
 const { v4: uuidv4 } = require('uuid');
 const codes = require('../src/cql-code-service');
 const cql = require('../src/cql');
@@ -20,10 +21,40 @@ const executor = (measure, patients, codeservice) => {
   console.log(result.patientResults); // eslint-disable-line no-console
   console.log(result.unfilteredResults); // eslint-disable-line no-console
 
-  fs.writeFile(
-    `./exec-files/results-${uuidv4()}.json`,
-    JSON.stringify(result),
-    err => err && console.error(err)
+  const removeArrayValues = patient => {
+    Object.entries(patient).forEach(([propertyKey, propertyValue]) => {
+      // remove property values that are arrays - the data pipeline doesn't need them
+      if (Array.isArray(propertyValue)) {
+        delete patient[propertyKey];
+      }
+    });
+  };
+
+  const cleanData = patientResults => {
+    Object.entries(patientResults).forEach(([patientKey, patientValue]) => {
+      const patient = patientValue;
+      // remove Patient data - not needed
+      delete patient.Patient;
+      patient.id = patientKey;
+
+      removeArrayValues(patient);
+    });
+  };
+
+  const convertToJSONL = err => {
+    if (err) {
+      console.error(err);
+    } else {
+      fs.createReadStream('./exec-files/results.json')
+        .pipe(jsonl())
+        .pipe(fs.createWriteStream(`./exec-files/results-${uuidv4()}.jsonl`));
+    }
+  };
+
+  cleanData(result.patientResults);
+
+  fs.writeFile('./exec-files/results.json', JSON.stringify(result.patientResults), err =>
+    convertToJSONL(err)
   );
 };
 
