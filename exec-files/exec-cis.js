52118ef70a8e0f1e74f7663a263b5ad2f7092cc8
tylernoblett <tyler.noblett@outlook.com>
2021-01-27 10:44:46 -0500

    change name to be easily found

--- exec-files/exec-cis.js
+++
@@ -1,6 +0,0 @@
-const codeservice = require('../data/codes/cis-codes');
-const executor = require('./exec-template');
-const measure = require('../json-elm/cis.json');
-const patients = require('../data/patients/cis-patients.json');
-
-executor(measure, patients, codeservice);
