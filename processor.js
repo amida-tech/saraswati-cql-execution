e890b37d99c43451ebab34247064341df212757c
hmayenve <henry@amida.com>
2021-04-16 16:51:49 -0400

    updating processor.js

--- processor.js
+++ processor.js
@@ -6,31 +6,33 @@ const { executeImmunization } = require('./exec-files/exec-childhood-immunizatio
 const { executeDepression } = require('./exec-files/exec-depression-screening');
 const { executeAsthma } = require('./exec-files/exec-medication-management-for-people-with-asthma');
 
-  watch('data/patients/', options = {'recursive': true }, function (event, filename) {
-    console.log(filename); // to know which file was processed
-    const patients = require('./' + filename);
-    if (patients) {
-      var data;
-      if (filename.startsWith("data/patients/a1c")) {
-        data = executeA1c(patients)
-      } else if (filename.startsWith("data/patients/asthma")) {
-        data = executeAsthma(patients)
-      }else if (filename.startsWith("data/patients/depression")) {
-        data = executeDepression(patients)
-      }else if (filename.startsWith("data/patients/diabetes")) {
-        data = executeDiabetes(patients)
-      }else if (filename.startsWith("data/patients/immunization")) {
-        data = executeImmunization(patients)
-      }
+watch('data/patients/', options = { 'recursive': true }, function (event, filename) {
+  console.log(filename); // to know which file was processed
+  const patients = require('./' + filename);
+  if (patients) {
+    let data;
+    if (filename.startsWith("data/patients/a1c")) {
+      data = executeA1c(patients)
+    } else if (filename.startsWith("data/patients/asthma")) {
+      data = executeAsthma(patients)
+    } else if (filename.startsWith("data/patients/depression")) {
+      data = executeDepression(patients)
+    } else if (filename.startsWith("data/patients/diabetes")) {
+      data = executeDiabetes(patients)
+    } else if (filename.startsWith("data/patients/immunization")) {
+      data = executeImmunization(patients)
+    }
+    if (data) {
       axios.post('http://127.0.0.1:5000/cql_service_connector', data).then(
         (response) => {
-            var result = response.data;
-            console.log(result);
+          var result = response.data;
+          console.log(result);
         },
         (error) => {
-            console.log(error);
+          console.log(error);
         }
-    );
-      
+      );
     }
-  });
+
+  }
+});
