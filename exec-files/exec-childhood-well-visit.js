e495468a9bec935ae5afb5317586b487f171894f
Eric <eawest2@gmail.com>
2021-06-30 10:08:03 -0400

    Updated with PR adjustments from henry

--- exec-files/exec-childhood-well-visit.js
+++ exec-files/exec-childhood-well-visit.js
@@ -1,7 +1,6 @@
 const codeservice = require('../data/codes/well-child-visits-15-mos-codes');
 const { execute } = require('./exec-template');
 const measure = require('../json-elm/well-child-visits-15-mos.json');
-//const patients = require('../data/patients/immunization/childhood-immunization-status-patients.json');
 
 const executeChildWellVisit = (patients) => {
     return execute(measure, patients, codeservice);
