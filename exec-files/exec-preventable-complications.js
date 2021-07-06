8873223f8330ae15936e2d1e11e306340936f402
jmfadeley <jmfadeley@gmail.com>
2021-07-06 07:55:59 -0400

    Tweaks.

--- exec-files/exec-preventable-complications.js
+++ exec-files/exec-preventable-complications.js
@@ -1,4 +1,4 @@
-const codeservice = require('../data/codes/preventable-complications');
+const codeservice = require('../data/codes/preventable-complications-codes');
 const { execute } = require('./exec-template');
 const measure = require('../json-elm/preventable-complications.json');
 //const patients = require('../data/patients/depression/depression-screening-follow-up.json');
