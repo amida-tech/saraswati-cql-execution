6a9eec925411617192a58e25d6cd611ac06093b4
jmfadeley <jmfadeley@gmail.com>
2021-09-24 16:28:35 -0400

    Backup of work before I break everything.

--- src/winston.js
+++ src/winston.js
@@ -1,20 +1,35 @@
 const { configuredFormatter } = require('winston-json-formatter');
 
-const {createLogger, transports } = require('winston');
+const {createLogger, transports, format } = require('winston');
 const pjson = require('../package.json');
 const config = require('../config');
 
-const logger = createLogger({ 
-  level: config.logLevel,
-  transports: [new transports.Console()]
-});
+let logger;
+if (config.env === 'production') {
+  logger = createLogger({ 
+    level: config.logLevel,
+    transports: [new transports.Console()]
+  });
 
-const options = { 
-  service: 'saraswati-cql-execution',
-  logger: 'application-logger',
-  version: pjson.version
-};
+  const options = { 
+    service: 'saraswati-cql-execution',
+    logger: 'application-logger',
+    version: pjson.version
+  };
 
-logger.format = configuredFormatter(options);
+  logger.format = configuredFormatter(options);
+} else {
+  logger = createLogger({
+    transports: [
+      new transports.File({
+        json: false,
+        filename:'log.log'
+      }),
+      new transports.Console({
+        format: format.simple(),
+      })
+    ],
+  });
+}
 
 module.exports = logger;
