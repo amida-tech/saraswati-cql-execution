4ac766aa908ae63f30d870a7b6480e76b43fff4c
Eric <eawest2@gmail.com>
2021-10-04 13:52:19 -0400

    buinsess logic and producer

--- processor-kafka.js
+++ processor-kafka.js
@@ -1,8 +1,57 @@
-// const consumer = require('../processor-kafka');
-// const { Kafka } = require('kafkajs');
+const kafka = require('./kafka');
 
-// See `processor-kafka-test.js` for working example. Steps for Henry:
-// 1. Start a consumer and listen to the topic from the config. 
-// 2. Run the measurements against each result, accumulating an object with all the measurements.
-// 3. Push this created object to its own produced topic. You will need producer for this.
-// That's it.
+const { kafkaConfig } = require("./config");
+
+const consumer = kafka.consumer({ groupId: 'hedis-measures' })
+
+const producer = kafka.producer()
+
+await consumer.connect()
+await producer.connect()
+
+await consumer.subscribe({ topic: kafkaConfig.kafkaConsumedTopic, fromBeginning: false })
+
+//Runs each time a message is recieved
+await consumer.run({
+    eachMessage: async ({ topic, partition, message }) => {
+        let fhirJson = message.value.toString();
+        let patients = JSON.parse(fhirJson);
+        let data = [];
+        evalData(patients,data)
+        if (data != null) {
+            producer.send(
+                {
+                    topic: kafkaConfig.kafkaProducedTopic,
+                    messages: [
+                        {value: JSON.stringify(data)},
+                    ],
+                }
+            )
+            console.log({
+                value: message.value.toString(),
+            })
+        }
+    },
+})
+
+// This function contains all of the business logic for the evaluation.
+function evalData(patients, data){
+    patients.forEach(element => {
+        let workingArray = [];
+        workingArray.push(executeA1c(element));
+        workingArray.push(executeAsthma(element));
+        workingArray.push(executeDepression(element));
+        workingArray.push(executeDiabetes(element));
+        workingArray.push(executeImmunization(element));
+        workingArray.push(executePPC(element));
+        workingArray.push(executePreventable(element));
+        workingArray.push(executeChildWellVisit(element));
+        workingArray.push(executeReadmission(element));
+
+        workingArray.forEach(score => {
+            if (score.Denominator != 0){
+                data.push(score)
+            }
+        });
+    });
+}
