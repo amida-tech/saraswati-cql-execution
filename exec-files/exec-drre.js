4b0476289620b2be236b043045cf51a6b26a6649
jmfadeley <jmfadeley@gmail.com>
2021-10-22 11:07:32 -0400

    [SAR-230] Working data.

--- exec-files/exec-drre.js
+++ exec-files/exec-drre.js
@@ -1,6 +1,6 @@
 const codeservice = require('../private/DRRE_HEDIS_MY2022-1.0.0/valuesets/drre');
 const { execute } = require('./exec-template');
-const measure = require('../private/DRRE_HEDIS_MY2022-1.0.0/elm/DRRE_HEDIS_MY2022-1.0.0.json');
+const measure = require('../private/drre-cql.json');
 
 const executeDepressionRemission = (patients) => {
   return execute(measure, patients, codeservice);
