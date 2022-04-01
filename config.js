253a8da0dda1f70db06200375ead7f2479528f58
Keith <keithjayjones@gmail.com>
2022-04-01 13:14:14 -0400

    Consumer groups can now be configured in the .env file (#66)
    * Consumer groups can now be configured in the .env file
    
    * Updated to use the measures we currently have test data for

--- config.js
+++ config.js
@@ -22,6 +22,7 @@ const envVarsSchema = Joi.object({
     .default('5001')
     .description('Port used for actuator endpoint'),
   KAFKA_BROKERS: Joi.string()
+    .default('redpanda1:9092')
     .description('The Kafka queue server addresses to connect to. We will parse the entry afterwards.'),
   KAFKA_USERNAME: Joi.string()
     .default('username1')
