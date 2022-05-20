67c20433a2aaf269d831236586b256a0581c57eb
Keith <keithjayjones@gmail.com>
2022-05-20 14:42:47 -0400

    Added checks for practitioner data

--- test/utilities/providerUtil-test.js
+++ test/utilities/providerUtil-test.js
@@ -10,15 +10,21 @@ describe('ProviderUtil test ', () => {
   it('AAB should find 2 providers', () => {
     const resultArray = createProviderList(aabData);
     resultArray.should.have.lengthOf(2);
+
+    resultArray[1].should.have.property('reference', 'urn:uuid:dca5e61c-59a3-3e2b-8a2e-1e02f64003fe');
   });
 
   it('COU should find 2 providers', () => {
     const resultArray = createProviderList(couData);
     resultArray.should.have.lengthOf(2);
+
+    resultArray[1].should.have.property('reference', 'urn:uuid:dca5e61c-59a3-3e2b-8a2e-1e02f64003fe');
   });
 
   it('IMA-E should find 3 providers', () => {
     const resultArray = createProviderList(imaeData);
     resultArray.should.have.lengthOf(3);
+
+    resultArray[2].should.have.property('reference', 'Practitioner/f005');
   });
 });
