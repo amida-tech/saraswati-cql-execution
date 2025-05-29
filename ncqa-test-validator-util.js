dbb324256d01fe08ede4058d7be9e3d76af66433
Keith <keithjayjones@gmail.com>
2025-05-29 16:58:27 -0400

    Validator for BCS-E works with different years

--- ncqa-test-validator-util.js
+++ ncqa-test-validator-util.js
@@ -416,8 +416,14 @@ const hedisData = {
       } else if (index === 5) { //BCSOT
         validPayor = data.support['Certification Medicare Other'];
       }
-      const age = measureFunctions.getAge(data);
-      return validPayor && age >= 52 && age <= 74 && data.gender.startsWith('f');
+      if (config.measurementYear === '2022') {
+        const age = measureFunctions.getAge(data);
+        return validPayor && age >= 52 && age <= 74 && data.gender.startsWith('f');
+      } else if (config.measurementYear === '2025') {
+        const age = measureFunctions.getAge(data);
+        return validPayor && age >= 42 && age <= 74 && data.result["Appropriate Gender For Screening"];
+      }
+      return false;
     },
     getAge: (data) => {
       let eventDate = new Date('2022-12-31');
